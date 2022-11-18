47f4e987ae9ad408ea37e56ed11f322803ccf353
CiciIsOk <86974163+CiciIsOk@users.noreply.github.com>
2022-11-18 12:23:49 +0400

    Update Skills.js

--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react'
 
-class Skills extends Component {
-    constructor(props)
-    {
-        super(props);
-        this.state={
-            'myskills':['HTML','CSS','JS','REACT JS']
-        };
-    }
+const Skills = () =>  {
 
-    render() {
+    const languages = ['English - Native or bilingual proficiency','Georgian - Native or bilingual proficiency','RussianRussian - Professional working proficiency']
+    const codingSkills = ['HTML', 'CSS', 'Javascript']
+    const frameworks = ['ReactJs', 'Redux', 'Git/Github', 'MaterialUI', 'Bootstrap',
+        'SASS', 'Jira', 'WebStorm', 'Figma',]
+
         return (
             <div className="condiv skills">
             <h1 className="subtopic">Skills</h1>
-            <ul>
-            {this.state.myskills.map((value)=>{
-                return <li>{value}</li>
-            })}
-            </ul>
+                <h3>Speaking Languages</h3>
+                <div style={{display:"flex",flexDirection:"column"}}>
+                    {languages.map((value)=>{
+                        return <p>{value},</p>
+                    })}
+                </div>
+
+                <h3>Coding Languages</h3>
+                <div style={{display:"flex",flexDirection:"row"}}>
+                    {codingSkills.map((value)=>{
+                        return <p>{value},</p>
+                    })}
+                </div>
+
+                <h3>Frameworks/Systems</h3>
+                <div style={{display:"flex",flexDirection:"row"}}>
+                    {frameworks.map((value)=>{
+                        return <p>{value},</p>
+                    })}
+                </div>
+
             </div>
             )
-        }
+
     }
     
     export default Skills
