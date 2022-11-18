e420d77a5c355b0353ee7c5f44cd003834e129e7
CiciIsOk <86974163+CiciIsOk@users.noreply.github.com>
2022-11-18 12:22:28 +0400

    Update Contact.js

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Social from '../components/Social';
-class Contact extends Component {
-    render() {
+
+const Contact = () => {
+
         return (
             <div className="condiv">
-            <h1 className="subtopic">Contact Me</h1>
+            <h1 className="subtopic">How To Reach Me :</h1>
             <h3>Email  :   tsitso.iobashvili.1@btu.edu.ge</h3>
             <h3>GitHub   :   @CiciIsOk</h3>
-            <h3>Instagram   :   @cici_07.10</h3>
+            <h3>LinkedIn   :   @Tsitsi Iobashvili</h3>
 
-            <Social />
+                <div style={{display:"flex",justifyContent:"center"}}> <Social /> </div>
             </div>
             )
-        }
+
     }
     
     export default Contact
-    
+   
