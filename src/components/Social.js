ebb3b8235235aa4436dec8c912511814d94464de
CiciIsOk <86974163+CiciIsOk@users.noreply.github.com>
2022-11-18 12:20:48 +0400

    Update Social.js

--- src/components/Social.js
+++ src/components/Social.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Social extends Component {
-    render() {
+const Social = () => {
         return (
             <div class="social">
-            <a href="https://github.com/CiciIsOk" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-            <a href="https://www.instagram.com/cici_07.10/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/cici.iobashvili" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
-            <a href="https://www.linkedin.com/in/tsitso-iobashvili-99546922b/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-            
+                <a href="https://github.com/CiciIsOk" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+                <a href="https://www.instagram.com/cici_07.10/" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/cici.iobashvili" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.linkedin.com/in/tsitsi-iobashvili-99546922b/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
             </div>
             )
         }
-    }
     
 export default Social
     
