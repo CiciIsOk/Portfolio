20ae5b94482ba45ca8e9a7bf3581ba808dd5706a
CiciIsOk <86974163+CiciIsOk@users.noreply.github.com>
2022-11-18 12:22:02 +0400

    Update About.js

--- src/contents/About.js
+++ src/contents/About.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react";
+import React from "react";
 
-class About extends Component {
-  render() {
+const About = () => {
     return (
       <div className="condiv">
         <h1 className="subtopic">About Me</h1>
         <h4>Hey there,</h4>
         <h1>I'm Tsitsi Iobashvili</h1>
         <h3>
-          Junior <u>front-End Developer</u>
+          <u>Front-End Developer</u>
         </h3>
         <br></br>
         <p>
-          I'm a hardworking and ambitious front-end (React) developer from
-          Georgia, who wants to learn as much as possible to enhance my skills
-          as an individual and also as a team worker. I always welcome new
-          challenges and opportunities, that help me overcome my limits. I
-          think, that developing web designs is a new generation of arts and
-          every developer should strive to create products they will be proud of
-          later.
+            I am Hardworking and ambitious front-end developer,
+            who wants to learn as much as possible to enhance
+            my skills as an individual and also as a team worker.
+            Always welcoming new challenges and opportunities,
+            that help me overcome my limits. Striving to create
+            products that will have positive impact on the users
+            and businesses.
         </p>
 
         
       </div>
     );
-  }
 }
 
 export default About;
